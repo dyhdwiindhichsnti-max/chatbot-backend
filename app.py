82fbc5857a84fd2c1cba45d9964bc3aeaba8aa5f
D14h123 <23421162@student.ubl.ac.id>
2026-01-04 13:35:19 +0700

    Add application file

--- app.py
+++ app.py
@@ -49,5 +49,6 @@ def chat():
     reply = get_response(user_message)
     return jsonify({"reply": reply})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
+
